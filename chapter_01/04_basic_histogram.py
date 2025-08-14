bfedead1b2a20fa838d95f2460d2c7d5f945d7d4
Edward Cheadle <echeadle@gmail.com>
2025-08-13 20:46:53 -0600

    finished with chapter 1

--- chapter_01/04_basic_histogram.py
+++ chapter_01/04_basic_histogram.py
@@ -1,16 +1,7 @@
 import streamlit as st
 import matplotlib.pyplot as pyplot
 import numpy as np
-from dotenv import load_dotenv
 
-load_dotenv()
-
-openai_api_key = os.getenv("OPENAI_API_KEY")
-if not openai_api_key:
-    st.error("The OPENAI_API_KEY environment variable is not set. Please add it to your .env file.")
-    st.stop()
-
-openai = OpenAI(api_key=openai_api_key)
 nums = np.random.normal(1, 1, size=5000)
 figure, subplot = pyplot.subplots()
 
