efff50a0d315193553e4870382ca846229a36092
Edward Cheadle <echeadle@gmail.com>
2025-08-16 11:59:06 -0600

    Finished chps in_progress_01-04, initial commit of 05

---
+++ chapter_06/in_progress_03/dashboard.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from data_loader import load_data
+from filter_panel import filter_panel
+
+
+
+data = load_data()
+filter_panel(data)
+st.write(data.head(5))
+
+
---
+++ chapter_06/in_progress_05/filter_panel.py
@@ -0,0 +1,12 @@
+import streamlit as st 
+from data_wrangling import get_unique_values
+
+filter_dims = ["Age group", "Gender", "Category", "Segment", "Product name", "State"]
+
+def filter_panel(df):
+    with st.expander("Filters"):
+        filter_cols = st.columns(len(filter_dims))
+        for idx, dim in enumerate(filter_dims):
+            with filter_cols[idx]:
+                unique_values = get_unique_values(df, dim)
+                st.multiselect(dim, unique_values)
