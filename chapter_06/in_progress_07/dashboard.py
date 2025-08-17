648d01682f84a51861d67ac097755fc3083a37f2
Edward Cheadle <echeadle@gmail.com>
2025-08-17 13:52:40 -0600

    Finished in_progress_07 in_progress_07

--- chapter_06/in_progress_07/dashboard.py
+++ chapter_06/in_progress_07/dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 from data_wrangling import get_filtered_data_within_date_range, prep_data
 from filter_panel import filter_panel
 from date_range_panel import date_range_panel
+from metric_bar import metric_bar
 
 st.set_page_config(layout='wide')
 
@@ -12,4 +13,7 @@ data = prep_data()
 filters = filter_panel(data)
 
 main_df = get_filtered_data_within_date_range(data, start, end, filters)
-st.write(main_df.head(5))
+if main_df.empty:
+  st.warning("No data to display")
+else:
+  metric_bar(main_df)
